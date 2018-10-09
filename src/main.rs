6c1235c1aa2e84918b2357a1592ba2d673b6a6c8
HOME\swkfn <sw.kfn4410uec@gmail.com>
2018-10-09 21:58:57 +0900

    コマンド解析のところを少し書いた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate clap;
+
+use std::io;
+use clap::{Arg};
+
+fn parse_command(){
+    let app = app_from_crate!()
+    .arg(Arg::from_usage("<TEXT> 'ファイルを指定してください.'"));
+    let matches = app.get_matches();
+
+    if let Some(o) = matches.value_of("TEXT") {
+        println!("Value for TEXT: {}", o);
+    }
+}
+
+fn main(){
+    parse_command();
     println!("Hello, world!");
 }
